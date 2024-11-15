7da4ca194ae6d680e2b8d1f1a0e31f3fd35e67b4
Bahir Hussain <mbahirh624@gmail.com>
2024-11-15 20:15:14 +0530

    test

--- src/main/java/com/IoT_basedMonitoringDashboard/IoT_basedMonitoringDashboard/Controller/EquipmentController.java
+++ src/main/java/com/IoT_basedMonitoringDashboard/IoT_basedMonitoringDashboard/Controller/EquipmentController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/api/equipment")
+@RequestMapping("/api/equipmen")
 public class EquipmentController {
 
     @Autowired
